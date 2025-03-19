527fa1bfe18e4f0cf4eba8e76d8f25b46678e4c0
Ethaniel <Ethaniel@100713.xyz>
2025-03-19 23:36:43 +0800

    路径错了

--- index_game.js
+++ index_game.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-    fetch("/api/Games_name.json")
+    fetch("./api/Games_name.json")
         .then(response => response.json())
         .then(games => {
             shuffleArray(games);
